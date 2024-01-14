e822a4122d679e6eb751582b281c2878121cc711
thachvu2410 <quangthach31@gmail.com>
2024-01-14 16:20:49 +0700

    first init

---
+++ src/main/java/com/main/cfAppBE/CfAppBeApplication.java
@@ -0,0 +1,13 @@
+package com.main.cfAppBE;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CfAppBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CfAppBeApplication.class, args);
+	}
+
+}
